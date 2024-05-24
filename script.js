e5f7edefefc6f7cdee7ef4062c1aac57517deb47
Oscar5p Kron <lakro001@live.se>
2024-05-24 13:04:08 +0000

    fixing my answer buttons and trying to get my text visuale on the first question

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
 const startButton = document.getElementById("start-btn");
 const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+
 console.log(startButton);
 console.log(document.getElementsByClassName("btn"));
 startButton.addEventListener("click", startGame);
 
+let shuffeledQuestions, currentQuestionIndex;
+
 function startGame() {
   console.log("Started");
   startButton.innerText = "Next!";
-  // startButton.classList.add('hide')
+  // startButton.classList.add("hide");
+  shuffeledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
   questionContainerElement.classList.remove("hide");
   setNextQuestion();
 }
 
-function setNextQuestion() {}
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffeledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+function resetState() {
+  nextButton.classList.add("hide");
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
 
 function selectAnswer() {}
 
 const questions = [
   {
-    question: "What is 2 + 2",
+    question: "What is 2 + 2?",
     answers: [
       { text: "4", correct: true },
       { text: "22", correct: false },
