1ed25eb017b1ea5095d5a81d03e09eff897ef3f3
Oscar5p Kron <lakro001@live.se>
2024-05-24 12:33:46 +0000

    adding my first question

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ function startGame() {
 function setNextQuestion() {}
 
 function selectAnswer() {}
+
+const questions = [
+  {
+    question: "What is 2 + 2",
+    answers: [
+      { text: "4", correct: true },
+      { text: "22", correct: false },
+    ],
+  },
+];
