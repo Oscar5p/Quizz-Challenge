1df5b9ef5b302fe5a2b78123f8cb02764173e9d6
Oscar5p Kron <lakro001@live.se>
2024-05-24 10:04:42 +0000

    adding log,elements and container to my start game function

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById
+('question-container')
 
-
+startButton.addEventListener('click', startGame)
 
 function startGame() {
-
+console.log('Started')
+startButton.classList.add('hide')
+questionContainerElement.classList.remove('hide')
+setNextQuestion()
 }
 
 function setNextQuestion() {
@@ -11,5 +16,5 @@ function setNextQuestion() {
 }
 
 function selectAnswer() {
-    
+
 }
