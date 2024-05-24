61cc76dc82cb46557cd1240fb17af2ce4acad2d3
Oscar5p Kron <lakro001@live.se>
2024-05-24 12:16:59 +0000

    added console logs, and changed text to my start button

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-const startButton = document.getElementById('start-btn')
-const questionContainerElement = document.getElementById
-('question-container')
-
-startButton.addEventListener('click', startGame)
+const startButton = document.getElementById("start-btn");
+const questionContainerElement = document.getElementById("question-container");
+console.log(startButton);
+console.log(document.getElementsByClassName("btn"));
+startButton.addEventListener("click", startGame);
 
 function startGame() {
-console.log('Started')
-startButton.classList.add('hide')
-questionContainerElement.classList.remove('hide')
-setNextQuestion()
+  console.log("Started");
+  startButton.innerText = "test";
+  // startButton.classList.add('hide')
+  questionContainerElement.classList.remove("hide");
+  setNextQuestion();
 }
 
-function setNextQuestion() {
+function setNextQuestion() {}
 
-}
-
-function selectAnswer() {
-
-}
+function selectAnswer() {}
