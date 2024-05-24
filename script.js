2b6634555b5d6f43f8339507e432d65c6eb08a5d
Oscar5p Kron <lakro001@live.se>
2024-05-24 13:22:27 +0000

    adding function clearstatusclass

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
 const questionContainerElement = document.getElementById("question-container");
 const questionElement = document.getElementById("question");
 const answerButtonsElement = document.getElementById("answer-buttons");
@@ -10,9 +11,7 @@ startButton.addEventListener("click", startGame);
 let shuffeledQuestions, currentQuestionIndex;
 
 function startGame() {
-  console.log("Started");
-  startButton.innerText = "Next!";
-  // startButton.classList.add("hide");
+  startButton.classList.add("hide");
   shuffeledQuestions = questions.sort(() => Math.random() - 0.5);
   currentQuestionIndex = 0;
   questionContainerElement.classList.remove("hide");
@@ -45,7 +44,28 @@ function resetState() {
   }
 }
 
-function selectAnswer() {}
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
 
 const questions = [
   {
